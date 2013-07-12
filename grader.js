fa69cee80a8266f80390adb3ddadbc266f805f1a
Khoa Tran <ktran9.lbs@gmail.com>
2013-07-12 18:10:49 +0000

    finished part 3

--- grader.js
+++ grader.js
@@ -24,8 +24,9 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
-var CHECKSFILE_DEFAULT = "checks.json";
+var CHECKSFILE_DEFAULT = "checks.json"
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -44,15 +45,27 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function($, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
-    return out;
+    console.log(JSON.stringify(out, null, 4));
+};
+
+var isValidUrl = function(url) {
+    return url.substr(0,4) === 'http';
+};
+
+var checkUrl = function(url, checksfile) {
+    if(isValidUrl(url)) {
+        rest.get(url).on('complete', function(result) {
+            var content = cheerio.load(result);
+            checkHtmlFile(content, checksfile);
+        });
+    }
 };
 
 var clone = function(fn) {
@@ -65,10 +78,14 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_in>', 'Link')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var checkJson;
+    if(program.url) {
+        checkJson = checkUrl(program.url, program.checks);
+    } else {
+        checkJson = checkHtmlFile(cheerioHtmlFile(program.file), program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
